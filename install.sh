284b24c6c285818abfe50cc6374a99294b6bbcca
W141-ID <40833448+W141-ID@users.noreply.github.com>
2024-12-16 19:52:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,4 +65,122 @@ function check_download {
 
     if [ ! -d "${K_SHAKETUNE_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain Shake&Tune module repository..."
-        git clone --branch swap-hack https://github.com/Frix-x/klippain-shaketune.git "${K_SHAK
+        git clone --branch swap-hack https://github.com/W141-ID/klippain-shaketune.git "${K_SHAKETUNE_PATH}"
+        if [ $? -eq 0 ]; then
+            chmod +x ${K_SHAKETUNE_PATH}/install.sh
+            printf "[DOWNLOAD] Download complete!\n\n"
+        else
+            echo "[ERROR] Download of Klippain Shake&Tune module repository failed!"
+            exit -1
+        fi
+    else
+        printf "[DOWNLOAD] Klippain Shake&Tune module repository already found locally. Continuing...\n\n"
+    fi
+}
+
+function setup_shaketune_sudo {
+    echo "[SETUP] Setting up sudo permissions for Shake&Tune swap file management..."
+    chmod +x ${K_SHAKETUNE_PATH}/install_swap_access.sh
+    ${K_SHAKETUNE_PATH}/install_swap_access.sh
+    printf "\n"
+}
+
+function setup_venv {
+    if [ ! -d "${KLIPPER_VENV_PATH}" ]; then
+        echo "[ERROR] Klipper's Python virtual environment not found!"
+        exit -1
+    fi
+
+    if [ -d "${OLD_K_SHAKETUNE_VENV}" ]; then
+        echo "[INFO] Old K-Shake&Tune virtual environment found, cleaning it!"
+        rm -rf "${OLD_K_SHAKETUNE_VENV}"
+    fi
+
+    source "${KLIPPER_VENV_PATH}/bin/activate"
+    echo "[SETUP] Installing/Updating K-Shake&Tune dependencies..."
+    pip install --upgrade pip
+    pip install -r "${K_SHAKETUNE_PATH}/requirements.txt"
+    deactivate
+    printf "\n"
+}
+
+function link_extension {
+    # Reusing the old linking extension function to cleanup and remove the macros for older S&T versions
+
+    if [ -d "${HOME}/klippain_config" ] && [ -f "${USER_CONFIG_PATH}/.VERSION" ]; then
+        if [ -d "${USER_CONFIG_PATH}/scripts/K-ShakeTune" ]; then
+            echo "[INFO] Old K-Shake&Tune macro folder found, cleaning it!"
+            rm -rf "${USER_CONFIG_PATH}/scripts/K-ShakeTune"
+        fi
+    else
+        if [ -d "${USER_CONFIG_PATH}/K-ShakeTune" ]; then
+            echo "[INFO] Old K-Shake&Tune macro folder found, cleaning it!"
+            rm -rf "${USER_CONFIG_PATH}/K-ShakeTune"
+        fi
+    fi
+}
+
+function link_module {
+    if [ ! -d "${KLIPPER_PATH}/klippy/extras/shaketune" ]; then
+        echo "[INSTALL] Linking Shake&Tune module to Klipper extras"
+        ln -frsn ${K_SHAKETUNE_PATH}/shaketune ${KLIPPER_PATH}/klippy/extras/shaketune
+    else
+        printf "[INSTALL] Klippain Shake&Tune Klipper module is already installed. Continuing...\n\n"
+    fi
+}
+
+function add_updater {
+    update_section=$(grep -c '\[update_manager[a-z ]* Klippain-ShakeTune\]' $MOONRAKER_CONFIG || true)
+    if [ "$update_section" -eq 0 ]; then
+        echo -n "[INSTALL] Adding update manager to moonraker.conf..."
+        cat ${K_SHAKETUNE_PATH}/moonraker.conf >> $MOONRAKER_CONFIG
+    fi
+}
+
+function restart_klipper {
+    echo "[POST-INSTALL] Restarting Klipper..."
+    sudo systemctl restart klipper
+}
+
+function restart_moonraker {
+    echo "[POST-INSTALL] Restarting Moonraker..."
+    sudo systemctl restart moonraker
+}
+
+
+printf "\n=============================================\n"
+echo "- Klippain Shake&Tune module install script -"
+printf "=============================================\n\n"
+
+
+# Run steps
+preflight_checks
+check_download
+setup_shaketune_sudo
+setup_venv
+link_extension
+link_module
+add_updater
+
+
+echo "[POST-INSTALL] Shake&Tune installation complete!"
+
+# Ask the user if they want to reboot now
+read -p "Do you want to reboot now? [y/N] " answer1
+case $answer1 in
+    [yY][eE][sS]|[yY])
+        sudo reboot
+        ;;
+    *)
+        # Ask the user if they still want to restart Klipper and Moonraker
+        read -p "Ok, but do you want to at least restart Klipper and Moonraker? [y/N] " answer2
+        case $answer2 in
+            [yY][eE][sS]|[yY])
+                restart_klipper
+                restart_moonraker
+                ;;
+            *)
+            echo "Ok, but just a heads-up: Shake&Tune won't be available until you restart your printer!"
+        esac
+        ;;        
+esac
